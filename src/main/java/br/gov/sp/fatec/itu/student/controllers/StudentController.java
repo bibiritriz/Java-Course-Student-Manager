cc65b031cb2992135f5ce9cab41ca05011e88901
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-05-27 11:10:34 -0300

    :wrench: fix: CORS error

--- src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -3,6 +3,7 @@ package br.gov.sp.fatec.itu.student.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.gov.sp.fatec.itu.student.entities.Student;
 import br.gov.sp.fatec.itu.student.services.StudentService;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("students")
 public class StudentController {
