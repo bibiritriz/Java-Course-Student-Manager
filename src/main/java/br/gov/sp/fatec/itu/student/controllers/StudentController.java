9ceda479d06cd4db926b22a7348c47fe97da62d6
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-05-27 10:58:37 -0300

    :sparkles: feat: Created controller, service, data.sql and methods PUT and DELETE

---
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -0,0 +1,45 @@
+package br.gov.sp.fatec.itu.student.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.gov.sp.fatec.itu.student.entities.Student;
+import br.gov.sp.fatec.itu.student.services.StudentService;
+
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+  @Autowired
+  private StudentService service;
+
+  @GetMapping
+  public ResponseEntity<List<Student>> getAll() {
+    return ResponseEntity.ok(service.getAll());
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PostMapping
+  public ResponseEntity<Student> save(@RequestBody Student student) {
+    return ResponseEntity.created(null).body(service.save(student));
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody Student student) {
+    service.update(student, id);
+    return ResponseEntity.noContent().build();
+  }
+}
---
+++ src/main/java/br/gov/sp/fatec/itu/student/services/StudentService.java
@@ -0,0 +1,39 @@
+package br.gov.sp.fatec.itu.student.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.gov.sp.fatec.itu.student.entities.Student;
+import br.gov.sp.fatec.itu.student.repositories.StudentRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class StudentService {
+  @Autowired
+  private StudentRepository repository;
+
+  public List<Student> getAll() {
+    return repository.findAll();
+  }
+
+  public void update(Student student, Long id) {
+    Student aux = repository.getReferenceById(id);
+
+    aux.setCourse(student.getCourse());
+    aux.setName(student.getName());
+
+    repository.save(aux);
+  }
+
+  public Student save(Student student) {
+    return repository.save(student);
+  }
+
+  public void delete(Long id) {
+    if (repository.existsById(id)) {
+      repository.deleteById(id);
+    } else {
+      throw new EntityNotFoundException("Aluno não encontrado");
+    }
+  }
+}
